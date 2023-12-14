3924a6e68b0097b3b6acda1cf3ffe811f56dcec6
Julien Carsique <julien.carsique@sonarsource.com>
2023-12-14 15:15:42 +0100

    BUILD-4306 invalidate CloudFront cache (#483)

--- po-generate-update-center-prod/upload.sh
+++ po-generate-update-center-prod/upload.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 : "${TRANSFER_DIR?}"
+: "${S3_BUCKET?}"
 
 S3_PATH=sonarqube/update/
 
@@ -9,3 +10,6 @@ echo "Upload from $TRANSFER_DIR to s3://$S3_BUCKET/$S3_PATH..."
 aws s3 sync "$@" --delete "$TRANSFER_DIR" "s3://$S3_BUCKET/$S3_PATH"
 rm -rf "$TRANSFER_DIR"
 echo "Upload done"
+
+DISTRIBUTION_ID=$(aws cloudfront list-distributions --query "DistributionList.Items[*].{id:Id,origin:Origins.Items[0].DomainName}[?starts_with(origin,'$S3_BUCKET')].id" --output text)
+aws cloudfront create-invalidation --distribution-id "$DISTRIBUTION_ID" --paths "/sonarqube/update/*" "/sonarqube/update/plugins/*"
