f841c50124184f975b60b6fbd5cf48fdafa6da1b
Julien Carsique <julien.carsique@sonarsource.com>
2024-06-14 10:11:33 +0200

    fix(BUILD-5327): find cloudfront distribution ID

--- po-generate-update-center-prod/upload.sh
+++ po-generate-update-center-prod/upload.sh
@@ -11,5 +11,6 @@ aws s3 sync "$@" --delete "$TRANSFER_DIR" "s3://$S3_BUCKET/$S3_PATH/"
 rm -rf "$TRANSFER_DIR"
 echo "Upload done"
 
-DISTRIBUTION_ID=$(aws cloudfront list-distributions --query "DistributionList.Items[*].{id:Id,origin:Origins.Items[0].DomainName}[?starts_with(origin,'$S3_BUCKET')].id" --output text)
+DISTRIBUTION_ID=$(aws cloudfront list-distributions --query "DistributionList.Items[*].{id:Id,origin:Origins.Items[].{DomainName:DomainName}[?starts_with(DomainName,'$S3_BUCKET')]}[?not_null(origin)].id" --output text)
+echo "Create CloudFront invalidation for distribution $DISTRIBUTION_ID"
 aws cloudfront create-invalidation --distribution-id "$DISTRIBUTION_ID" --paths "/$S3_PATH/*" "/$S3_PATH/plugins/*"
